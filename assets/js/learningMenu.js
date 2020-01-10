2ef9ec05c3c8f2003b73c5a4f1c8475b25015708
PierreMonvoisin <monvoisin.pierre@gmail.com>
2020-01-10 14:53:11 +0100

    Ajout de la modale Paramètre mais bug d'affichage

--- assets/js/learningMenu.js
+++ assets/js/learningMenu.js
@@ -1,16 +1,20 @@
 $(function () {
+  // Enable tooltip
   $('[data-toggle="tooltip"]').tooltip();
+  // Zoom on the button on list mousover
   $('#default li').mouseover(function(){
     var classList = $(this)[0].className.split(' ');
     $("#" + classList[0]).css({'transition': '0.5s', 'transform': 'scale(1.1)'});
   })
+  // Zoom back to initial on mouseleave
   $('#default li').mouseleave(function(){
     var classList = $(this)[0].className.split(' ');
     $("#" + classList[0]).css({'transition': '0.5s', 'transform': 'scale(1)'});
   })
+  // Recognition of which button was pushed and launch the right function
   $('button').click(function () {
     var whichButton = $(this).attr('id'), buttonName = [];
-    buttonName = whichButton.split(/(?=[A-Z])/);
+    var buttonName = whichButton.split(/(?=[A-Z])/);
     // Detection of which button was pressed
     if (buttonName[0] === 'Top' && buttonName[1] === 'Left') {
       // Top left
--- learningMenu.php
+++ learningMenu.php
@@ -19,13 +19,14 @@
   <div id="mainMenu" class="container-fluid p-0">
     <div class="row w-100 p-0 m-0">
       <div class="col p-0 m-0">
-        <button id="TopLeft" type="button" class="btn bg-blue" data-toggle="tooltip"
-        data-placement="bottom" title="Comment résoudre un 3x3"><img src="https://i.pinimg.com/originals/4f/37/4c/4f374c5803ccb759755066a8e887e623.png" alt="3x3 cube"></button>
-        <button id="BottomLeft" type="button" class="btn btn-light" data-toggle="tooltip"
-        data-placement="top" title="Paramètres"><img src="https://image.flaticon.com/icons/svg/1242/1242443.svg" alt="Settings"> </button>
+          <!--Top Left Button-->
+        <button id="TopLeft" type="button" class="btn bg-blue" title="Comment résoudre un rubik's cube"><img src="https://i.pinimg.com/originals/4f/37/4c/4f374c5803ccb759755066a8e887e623.png" alt="3x3 cube"></button>
+          <!--Bottom Left Button-->
+          <button id="BottomLeft" type="button" class="btn btn-light" data-toggle="modal" data-target="#settings" title="Paramètres"><img src="https://image.flaticon.com/icons/svg/1242/1242443.svg" alt="Settings"></button>
       </div>
       <div id="mainLesson" class="col-9">
         <ul id="default" class="list-group shadow">
+            <!--! Always keep the " TopLeft " class in first !-->
           <li class="TopLeft list-group-item bg-blue text-center text-white sectionTitle m-0 py-0">Comment résoudre un rubik's cube</li>
           <li class="TopLeft list-group-item list-group-item-primary">Morbi leo risus</li>
           <li class="TopRight list-group-item bg-green text-center text-white sectionTitle m-0 py-0">Algorithmes et conseils pour s'améliorer</li>
@@ -37,12 +38,32 @@
         </ul>
       </div>
       <div class="col p-0 ml-auto mr-0 my-0">
-        <button id="TopRight" type="button" class="btn bg-green" data-toggle="tooltip"
+          <!--Top Right Button-->
+          <button id="TopRight" type="button" class="btn bg-green" data-toggle="tooltip"
         data-placement="bottom" title="Algorithmes et conseils"><img src="http://static1.squarespace.com/static/54f2df67e4b079e94c291e4f/t/54f700b9e4b06512fa5f4528/1425473725865/rubiks+cube+corner+permuted?format=1500w" alt="Algorithms"></button>
-        <button id="BottomRight" type="button" class="btn bg-yellow" data-toggle="tooltip"
+          <!--Bottom Right Button-->
+          <button id="BottomRight" type="button" class="btn bg-yellow" data-toggle="tooltip"
         data-placement="top" title="Rechercher dans les lessons"><img src="https://image.flaticon.com/icons/svg/639/639375.svg" alt="Search"></button>
       </div>
     </div>
+  </div>
+  <div class="modal fade" id="settings" tabindex="-1" role="dialog" aria-labelledby="settings" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalCenteredLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  ...
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
   </div>
   <?php // Local storage authorization modal
   include 'userAuthorization.php'; ?>
