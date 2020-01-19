fcc05eb7ade9d19b69ee60f8abd3aebfe7522b1a
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2020-01-19 20:25:47 +0100

    début scroll header display

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,14 +1,21 @@
 $(function(){
-  var mouseX, mouseY;
+  var mouseX, mouseY, closing;
   // Check mouse position to trigger top navigation tabs
   $(document).mousemove(function(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
-    if ( mouseY < 10) {
+    if ( mouseY < 20) {
       $('header').slideDown('fast');
+    } else if ( < 20 mais > header + 20){
+
+    } else if ( mouseY > ($('header').height() + 20)) {
+      closing = setTimeout(function(){ $('header').slideUp('slow'); }, 2000);
     }
-    if ( mouseY > ($('header').height() + 20)) {
-      $('header').slideUp('slow');
+  });
+  $(document).bind('mousewheel', function(e){
+    if (e.originalEvent.wheelDelta / 120 > 0) {
+      console.log('up' + $(document).scrollTop());
+      $('header').slideDown('fast');
     }
   });
 });
