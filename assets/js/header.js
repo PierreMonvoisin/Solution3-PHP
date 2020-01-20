b80a2499369216aef45e3968035d22feef280995
PierreMonvoisin <monvoisin.pierre@gmail.com>
2020-01-20 09:23:16 +0100

    Avancé sur l'UX du header, encore bugé

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,15 +6,15 @@ $(function(){
     mouseY = e.pageY;
     if ( mouseY < 20) {
       $('header').slideDown('fast');
-    } else if ( < 20 mais > header + 20){
-
+    } else if ( mouseY >= 20 && mouseY <= ($('header').height() + 20)){
+      clearTimeout(closing);
     } else if ( mouseY > ($('header').height() + 20)) {
       closing = setTimeout(function(){ $('header').slideUp('slow'); }, 2000);
     }
   });
   $(document).bind('mousewheel', function(e){
     if (e.originalEvent.wheelDelta / 120 > 0) {
-      console.log('up' + $(document).scrollTop());
+      console.log('up by ' + $(document).scrollTop());
       $('header').slideDown('fast');
     }
   });
