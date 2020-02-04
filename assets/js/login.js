61eac54ed5c3ea82b5622c364ec4af7c1d083ac3
Monvoisin <monvoisin.pierre@gmail.com>
2020-02-04 16:55:17 +0100

    Push de fin de journée

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,23 @@
-$(function () {
-    // Change avatar image on mouse hover
-    $('#avatarContainer').mouseenter(function () {
-        $('#topAvatar').attr('src', 'assets/img/addPhotoAvatar.png');
+$(function(){
+  // Change avatar image on mouse hover
+  $('#avatarContainer').mouseenter(function(){
+    $('#topAvatar').attr('src', 'assets/img/addPhotoAvatar.png');
+  })
+  $('#avatarContainer').mouseleave(function(){
+    $('#topAvatar').attr('src', 'https://image.flaticon.com/icons/svg/163/163801.svg');
+  })
+  $('input').blur(function(){
+    checkInputs();
+  })
+  // Check all form inputs
+  function checkInputs() {
+    var errorLog = [], valueLog = [], validity = false;
+    $('#newUserCard :input[class=form-control]').each(function(){
+      if ($(this).val() != ''){
+        valueLog += $(this).val();
+      } else {
+        errorLog += $(this).val();
+      }
     });
-    $('#avatarContainer').mouseleave(function () {
-        $('#topAvatar').attr('src', 'https://image.flaticon.com/icons/svg/163/163801.svg');
-    });
-    // Form verification
-
+  }
 });
