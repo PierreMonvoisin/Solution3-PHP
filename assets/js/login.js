d10620a34a55d73cc758f503403640e88119fa9d
Monvoisin <monvoisin.pierre@gmail.com>
2020-02-05 20:33:32 +0100

    Nettoyage du formulaire, suppresion des variables inutiles et début de la 'validation' des valeurs

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,63 +6,55 @@ $(function(){
   $('#avatarContainer').mouseleave(function(){
     $('#topAvatar').attr('src', 'https://image.flaticon.com/icons/svg/163/163801.svg');
   })
-  var message = 'ERROR';
-  var errorLog, valueLog, full, form;
+  var message = 'ERROR', validity = false;
+  var errorLog = [], formInfos;
   // Check all form inputs
   function checkInputs() {
-    errorLog = [], valueLog = [], full = false, validity = false, form = 'default';
+    errorLog = [], validity = false;
     // Check for the form to create a new user
     $('#newUserCard :input[class=form-control]').each(function(){
-      // Loop though all inputs and get their value
-      if ($(this).val() != ''){
-        valueLog.push($(this).attr('id'));
-      } else {
-        // Check if input is file-input, if yes, default add it to the set value log
-        $(this).attr('id') === 'file-input' ? valueLog.push($(this).attr('id')) : errorLog.push($(this).attr('id'));
+      if (($(this).val()).trim() === ''){
+        if ( $(this).attr('id') != 'file-input' ){ errorLog.push($(this).attr('id')) };
       }
-      // Change message depending on the number of value gathered
-      if (valueLog.length < 5 && errorLog.length >= 0){
+      if (errorLog.length > 0){
         message = 'Veuillez renseigner tous les champs';
       } else {
         message = 'Merci d\'avoir renseigné tous les champs';
-        full = true;
-        form = 'newUser';
+        validity = true;
       }
-      return valueLog, errorLog;
+      formInfos = [errorLog, validity, message];
+      return formInfos;
     });
+    var [errorLog, validity, message] = formInfos;
     // If the new user form is empty, check the login form
-    if (valueLog.length == 0 && (errorLog.length == 5 || errorLog.length == 0)){
-      // Reset the error and value logs;
-      errorLog = [], valueLog = [];
+    if (errorLog.length == 0){
+      // Reset the error logs
+      errorLog = [];
       $('#loginCard :input[class=form-control]').each(function(){
-        if ($(this).val() != ''){
-          valueLog.push($(this).attr('id'));
-        } else {
+        if (($(this).val()).trim() === ''){
           errorLog.push($(this).attr('id'));
         }
-        if (valueLog.length < 2 && errorLog.length >= 0){
+        if (errorLog.length > 0){
           message = 'Veuillez renseigner tous les champs';
         } else {
           message = 'Merci d\'avoir renseigné tous les champs';
-          full = true;
-          form = 'login';
+          validity = true;
         }
       });
     }
     // Retourne les informations récoltées dans les formulaires
-    return [form, full, message, valueLog, errorLog];
+    formInfos = [errorLog, validity, message];
+    return formInfos;
   }
   $('.card input').blur(function(){
     var results = checkInputs();
     console.log('final result is :');
     console.log(results);
-    var formName = results[0], status = results[1], message = results[2];
-    var values = results[3], errors = results[4];
+    var [errors, status, message] = results;
     if (status && errors.length == 0){
-      validity = true;
-      
+      alert(':D');
     } else if (errors.length > 0){
-      alert('proot');
+      alert(':C');
     }
   })
 });
